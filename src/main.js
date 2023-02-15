843be739ab8c5cc2ef5d89cecb73e8612e199a4e
Jakub Wachowiak <104766988+notfirsttimecoding@users.noreply.github.com>
2023-02-15 22:37:52 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+const app = createApp(App);
+
+//use immer erste
+app.use(PrimeVue);
+
+app.component('Button', Button);
+app.component('DataTable', DataTable);
+app.component('Column', Column)
+
+//mount immer letzte
+app.mount('#app');
